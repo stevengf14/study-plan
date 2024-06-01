797489930c94ab900c23a2f6a451f77e41bdcc09
Steven Guamán <stevengf14@gmail.com>
2024-05-31 23:01:20 -0500

    Create solution.js

---
+++ Increasing Triplet Subsequence/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function (nums) {
+    var result = false;
+    const n = nums.length;
+
+    let first = Number.MAX_SAFE_INTEGER;
+    let second = Number.MAX_SAFE_INTEGER;
+
+    for (let num of nums) {
+        if (num <= first) {
+            first = num;
+        } else if (num <= second) {
+            second = num;
+        } else {
+            return true;
+        }
+    }
+    return result;
+}
