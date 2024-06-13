0a87e45fe8201524394b365ea6bc871a6f3042e7
Steven Guamán <stevengf14@gmail.com>
2024-06-12 21:13:57 -0500

    Create Solution.java

---
+++ Maximum Average Subrray/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+        int maxSum = currentSum;
+        for (int i = k; i < nums.length; i++) {
+            currentSum = currentSum - nums[i - k] + nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        return (double) maxSum / k;
+    }
+}
